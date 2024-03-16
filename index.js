7b3c3e5c735e95e1854d641d901efcc11fcfa928
JimJam117 <jamessparrow101@gmail.com>
2024-03-16 23:29:00 +0000

    initial setup

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import {dirname} from 'path';
+import { fileURLToPath } from 'url';
+
+// Get current directory
+const current_dir = dirname(fileURLToPath(import.meta.url));
+
+// Env variables
+const port = 5000;
+
+// Start server
+const app = express();
+
+// Basic route
+app.get('/', (req, res) => {
+    res.sendFile(current_dir + '/frontend/index.html');
+})
+
+// Listen on port
+app.listen(port, () => {console.log(`App started on port ${port}`)});
