22f531a687a615871fe482845ca02f965e502aec
Talan Oates <fvt20npu@uea.ac.uk>
2024-04-18 16:59:24 +0100

    inital commit on new branch as I need to disable some of the signup code for quick testing

--- scripts/sanitiseSQL.js
+++ scripts/sanitiseSQL.js
@@ -3,11 +3,15 @@ export default function sanitiseSQL(input) {
     try {
 
         // TODO: implement sanitisation, right now this is just returns the input
+
         let sanitisedInput = input
+        if (sanitisedInput.includes(`'`)) {
+            console.log("includes check mark")
+        }
 
         return sanitisedInput
 
-    } 
+    }
     catch (error) {
         console.log("Error Sanitising input:", error)
     }
